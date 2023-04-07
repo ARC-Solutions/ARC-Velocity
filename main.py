c10ab9f540607b7f8af1d29aff8b5f1cf15868ae
RickyRAV <rickyraveanu02@gmail.com>
2023-04-07 14:15:50 +0200

    INIT
    Co-Authored-By: Nippon <102690989+Noppin@users.noreply.github.com>
    Co-Authored-By: Nikola <118454864+Niukuu@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,35 @@
+import serial
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+from concurrent.futures import ThreadPoolExecutor
+import os
+
+REACT_BUILD_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), './build')
+
+app = Flask(__name__, static_folder='./src', template_folder='./public')
+CORS(app)
+
+executor = ThreadPoolExecutor(max_workers=1)
+f1 = executor.submit(app.route, '/home')
+f1.result()
+
+@app.route('/', defaults={'path': ''})
+
+@app.route('/forward', methods=['POST'])
+def forward():
+    try:
+        print('car go forward')
+        return jsonify({'result': 'forward'})
+    except serial.SerialTimeoutException:
+        return jsonify({'error': 'Serial Timeout'})
+
+@app.route('/backward', methods=['POST'])
+def backward():
+    try:
+        print('car go backwar')
+        return jsonify({'error': 'backward'})
+    except serial.SerialTimeoutException:
+        return jsonify({'error': 'Serial Timeout'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
