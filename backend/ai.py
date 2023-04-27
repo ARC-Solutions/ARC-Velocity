7b90352be3e40f715c8a16dcf88b23b85308acd5
RickyRAV <rickyraveanu02@gmail.com>
2023-04-27 21:55:20 +0200

    AddedTrainingData&Tensorflow&Keras Integration

--- backend/ai.py
+++ backend/ai.py
@@ -1,11 +1,11 @@
-# Import additional libraries
 import cv2
 import numpy as np
-import time
-import urllib.request
-from urllib.error import URLError, HTTPError
+import tensorflow as tf
 from arduino_Connection import arduino as ser
+from model_utils import preprocess_image
 
+model_path = r"D:\priv\Programming\ARCV2\Model"
+model = tf.keras.models.load_model(model_path)
 
 def send_command(command):
     ser.write(f"{command}\n".encode())
@@ -20,66 +20,38 @@ def start_ai_car(arduino_serial):
         if not ret:
             break
 
-        # Preprocess the frame (convert to grayscale and apply Gaussian blur)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        # Define the region of interest
+        height, width, _ = frame.shape
+        roi_left = width // 3
+        roi_right = width * 2 // 3
+        roi_top = height // 3
+        roi_bottom = height * 2 // 3
 
-        # Apply edge detection
-        edges = cv2.Canny(blurred, 50, 150)
+        # Draw the ROI rectangle on the frame
+        cv2.rectangle(frame, (roi_left, roi_top), (roi_right, roi_bottom), (0, 255, 0), 2)
 
-        # Define a region of interest (ROI)
-        height, width = edges.shape
-        mask = np.zeros_like(edges)
-        roi_corners = np.array([[
-            (0, height - 85),
-            (width // 3, height - 1.5 * height // 3),
+        # Extract the region of interest from the frame
+        roi_frame = frame[roi_top:roi_bottom, roi_left:roi_right]
 
-            (2 * width // 3, height - 1.5 * height // 3),
-            (width, height - 85)
-        ]], dtype=np.int32)
+        # Preprocess the frame for the model
+        preprocessed_frame = preprocess_image(frame)
+        preprocessed_frame = np.expand_dims(preprocessed_frame, axis=0)
 
-        cv2.fillPoly(mask, roi_corners, 255)
-        roi = cv2.bitwise_and(edges, mask)
+        # Use the model's prediction to decide the direction to move
+        prediction = model.predict(preprocessed_frame)
+        predicted_label = np.argmax(prediction)
 
-        # Find lines using the Hough transform
-        lines = cv2.HoughLinesP(roi, 1, np.pi / 180, 20, np.array([]), minLineLength=5, maxLineGap=2)
+        # Preprocess the ROI frame for the model
+        preprocessed_frame = preprocess_image(roi_frame)
+        preprocessed_frame = np.expand_dims(preprocessed_frame, axis=0)
 
-        if lines is not None:
-            # Filter out lines with a zero difference in their x-coordinates
-            valid_lines = [line for line in lines for x1, y1, x2, y2 in line if (x2 - x1) != 0]
+        if predicted_label == 1:
+            send_command("forward_on")
+        else:
+            send_command("forward_off")
+            send_command("right_off")
+            send_command("left_off")
 
-            if valid_lines:
-                # Draw the detected lines on the original frame
-                for line in valid_lines:
-                    for x1, y1, x2, y2 in line:
-                        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-                # Calculate the average slope and position of the lines
-                slope_avg = np.nanmean([(y2 - y1) / (x2 - x1) for line in valid_lines for x1, y1, x2, y2 in line])
-                x_avg = np.mean([(x1 + x2) / 2 for line in valid_lines for x1, y1, x2, y2 in line])
-
-                # Decide on the direction to move
-                if slope_avg > 0:
-                    if x_avg < width // 2:
-                        send_command("right_on")
-                        send_command("left_off")
-                    else:
-                        send_command("right_off")
-                        send_command("left_on")
-                else:
-                    if x_avg < width // 2:
-                        send_command("right_on")
-                        send_command("left_off")
-                    else:
-                        send_command("right_off")
-                        send_command("left_on")
-                    send_command("forward_on")
-            else:
-                send_command("forward_off")
-                send_command("right_off")
-                send_command("left_off")
-        # Draw the ROI polygon on the frame
-        cv2.polylines(frame, roi_corners, True, (0, 255, 255), 2)
 
         # Show the processed video feed
         cv2.imshow("processed_frame", frame)
